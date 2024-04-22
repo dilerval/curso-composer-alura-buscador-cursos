99cb091bc8c507a2846623e75f980dcc0fae2a03
Dilerval Carvalho Silva <dilerval@dilerval.com.br>
2024-04-22 13:34:53 -0300

    02. 06 Buscando os cursos da Alura

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,8 +1,18 @@
 <?php
-
+require 'vendor/autoload.php';
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
-$resposta = $client->request('GET', 'https://cursos.alura.com.br/formacao-linguagem-php');
+$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 
 $html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('.card-curso__nome');
+//var_dump($cursos);
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,6 +20,7 @@ class ComposerStaticInit183d3635e7b80e96a5db830cf4937a41
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Component\\DomCrawler\\' => 29,
+            'Symfony\\Component\\CssSelector\\' => 30,
         ),
         'P' => 
         array (
@@ -50,6 +51,10 @@ class ComposerStaticInit183d3635e7b80e96a5db830cf4937a41
         'Symfony\\Component\\DomCrawler\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/dom-crawler',
+        ),
+        'Symfony\\Component\\CssSelector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/css-selector',
         ),
         'Psr\\Http\\Message\\' => 
         array (
