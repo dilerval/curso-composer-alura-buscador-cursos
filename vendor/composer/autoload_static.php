1c7f27566becc675b9531e4c5dbec9b35e9e4e23
Dilerval Carvalho Silva <dilerval@dilerval.com.br>
2024-04-26 14:26:29 -0300

    05. 04 Compondo Scripts

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -95,8 +95,8 @@ class ComposerStaticInit183d3635e7b80e96a5db830cf4937a41
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Webmozart\\Assert\\' => 
         array (
